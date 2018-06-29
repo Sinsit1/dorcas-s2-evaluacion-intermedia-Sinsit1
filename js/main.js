afcf72af1ce2b4da5272392e7507805a6af17c21
sinsit1 <alexandra8989@gmail.com>
2018-06-29 18:16:20 +0200

    done Running game

--- js/main.js
+++ js/main.js
@@ -1,3 +1,42 @@
 'use strict';
 
-console.log('>> Ready :)');
+// creo un numero random:
+
+var numeroAdivinar=0;
+
+function getRandomNumber(max) {
+  return Math.ceil(Math.random() * max);
+}
+
+// guardo el numero a adivinar
+
+numeroAdivinar = getRandomNumber(100);
+
+
+console.log(numeroAdivinar);
+
+//recojo datos del input
+
+var evento;
+var button = document.querySelector(".button");
+var feedback = document.querySelector(".feedback");
+
+function recogerInput () {
+
+  var entradaDatos;
+  var input = document.querySelector('.addValor');
+  entradaDatos = input.value;
+  entradaDatos = parseInt(entradaDatos);
+  console.log (entradaDatos);
+  if (entradaDatos === numeroAdivinar){
+    feedback.innerHTML = 'FELICIDADES!! HAS GANADO!';
+  } else if (entradaDatos> numeroAdivinar){
+    feedback.innerHTML = 'Te has pasado, intenta un numero menor!';
+  }else {
+    feedback.innerHTML = 'Te has quedado corta, intenta un numero mayor!';
+  }
+}
+
+button.addEventListener('click', recogerInput);
+
+// comparo mi numero random con el numero que he recogido //
